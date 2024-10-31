d940c2f5a8f718e5398f409509dbd5dffa22544c
Damscode <139973432+damscode-dev@users.noreply.github.com>
2024-10-31 16:24:02 -0400

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import BudgetCalculator from '../components/BudgetCalculator'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <BudgetCalculator />
+    </main>
+  )
+}
